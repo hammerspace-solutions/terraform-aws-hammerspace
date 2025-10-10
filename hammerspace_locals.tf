360eb937352dd50853b6cc947df8e42a3e997c38
Michael Kade <mike@mikda.com>
2025-10-10 11:58:23 -0700

    profile(s) must compare for null

--- hammerspace_locals.tf
+++ hammerspace_locals.tf
@@ -73,8 +73,8 @@ locals {
   provides_key_name      = var.common_config.key_name != null && var.common_config.key_name != ""
   enable_iam_admin_group = var.iam_user_access == "Enable"
   create_iam_admin_group = local.enable_iam_admin_group && (
-    var.iam_profile_group || var.iam_profile_group == "")
-  create_profile         = var.iam_profile_name || var.iam_profile_name == ""
+    var.iam_profile_group == null || var.iam_profile_group == "")
+  create_profile         = var.iam_profile_name == null || var.iam_profile_name == ""
   dsx_add_volumes_bool   = local.should_create_any_anvils && var.dsx_add_vols
 
   # --- Mappings & Derived Values ---
